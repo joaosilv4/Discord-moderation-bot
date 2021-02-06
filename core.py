07f33634eec7a278b5f0ce6f24c0b2cd5570f965
joaosilv4 <78647637+joaosilv4@users.noreply.github.com>
2021-02-06 14:57:36 +0000

    Update core.py

--- core.py
+++ core.py
@@ -48,26 +48,6 @@ class Core(commands.Cog):
         await ctx.send(f'Pong! {latency}ms')
         
 #--------------------------------------------------------------------------------
-        
-    # Info command
-    # Used for displaying information about the bot
-    @commands.command()
-    async def info(self, ctx):
-        embed = discord.Embed(title="Information about the Terabyte Bot")
-        embed.color = random.randint(0, 0xffffff)
-        pythonVersion = str(sys.version).split(" ")
-        pythonVersion = pythonVersion[0]
-        embed.add_field(name="Python", value=f"[{pythonVersion}](https://www.python.org)")
-        embed.add_field(name="Discord.Py", value=f"[{str(discord.__version__)}](https://https://discordpy.readthedocs"
-                                                 f".io)")
-        embed.add_field(name="Bot Version", value=f"[1.0.0](https://www.coventry.ac.uk)")
-        embed.add_field(name="About Terabyte", value="Terabyte was made by 5 University Students who are "
-                                                       "studying Computer Science at Coventry University.\n\nIt was "
-                                                       "made for a University Project and since than has been "
-                                                       "improved and made into a new bot.", inline=False)
-        embed.set_footer(text="Created by Adil, Joao, Nick, Ibra & Leo")
-        await ctx.send(embed=embed)
-        
 
 # Setup function required by discord.py
 def setup(bot):
