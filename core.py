3f4e2e2a8b9f2594a80eb3eab94a673b1cca6884
joaosilv4 <78647637+joaosilv4@users.noreply.github.com>
2021-02-06 14:57:03 +0000

    Update core.py

--- core.py
+++ core.py
@@ -38,13 +38,17 @@ class Core(commands.Cog):
                 await message.channel.send(word.capitalize() + ' ' + message.author.name)
                 break
 
+#--------------------------------------------------------------------------------               
+                
     # Ping command
     # Used for testing the latency of the bot and whether it is working
     @commands.command()
     async def ping(self, ctx):
         latency = round(self.bot.latency * 1000)
         await ctx.send(f'Pong! {latency}ms')
-
+        
+#--------------------------------------------------------------------------------
+        
     # Info command
     # Used for displaying information about the bot
     @commands.command()
@@ -63,32 +67,7 @@ class Core(commands.Cog):
                                                        "improved and made into a new bot.", inline=False)
         embed.set_footer(text="Created by Adil, Joao, Nick, Ibra & Leo")
         await ctx.send(embed=embed)
-
-    # Help command
-    # Used for displaying help about the bot
-    @commands.command(name="help", pass_context=True)
-    async def help_command(self, ctx):
-        with open('prefixes.json', 'r') as f:
-            prefixes = json.load(f)
-
-        prefix = prefixes[(str(ctx.guild.id))]
-        embed = discord.Embed(title="Game Stats Help")
-        embed.color = random.randint(0, 0xffffff)
-        embed.add_field(name=f"{prefix}help", value="Displays this command")
-        embed.add_field(name=f"{prefix}info", value='Displays info about the bot')
-        embed.add_field(name=f"{prefix}ping", value="Pings the bot")
-        embed.add_field(name=f"{prefix}cog load <cog>", value="Load a cog into the bot")
-        embed.add_field(name=f"{prefix}cog unload <cog>", value="Unload a cog from the bot")
-        embed.add_field(name=f"{prefix}cog reload <cog>", value="Reload a cog from the bot")
-        embed.add_field(name=f"{prefix}gamelobby create <maxPlayers> <game>", value="Create a game lobby")
-        embed.add_field(name=f"{prefix}gamelobby delete <lobbyName>", value="Delete a game lobby (Requires Admin Perms)")
-        embed.add_field(name=f"{prefix}gamelobby setcategory <categoryID>", value="Set game lobby category (Requires "
-                                                                                  "Admin Perms)")
-        embed.add_field(name=f"{prefix}ytt", value="Checks currently trending games on YouTube")
-        embed.set_footer(text="Created by Adil, Joao, Nick, Ibra & Leo")
-
-        await ctx.send(embed=embed)
-
+        
 
 # Setup function required by discord.py
 def setup(bot):
